3a74ee65dc0129cb47d5efc1084c47f21797a67d
rentianxiang <285032439@qq.com>
2022-01-23 13:59:59 +0800

    Import image and convert it to 2d array

--- pixel.js
+++ pixel.js
@@ -16,12 +16,16 @@ class PixelImage {
     }
 
     #initData() {
-        this.data = [["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#21212b", "#21212b", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#c0e49c", "#c0e49c", "#c0e49c", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#e74c3e", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#ee7a61", "#e74c3e", "#e74c3e", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#ee7a61", "#ee7a61", "#e74c3e", "#e74c3e", "#e74c3e", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#ee7a61", "#ee7a61", "#000000", "#cccccc", "#e74c3e", "#e74c3e", "#e74c3e", "#21212b", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#ee7a61", "#ee7a61", "#000000", "#000000", "#e74c3e", "#e74c3e", "#21212b", "#eac18b", "#21212b", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#ee7a61", "#ee7a61", "#ee7a61", "#ee7a61", "#ee7a61", "#e74c3e", "#21212b", "#eac18b", "#eac18b", "#ebd4b8", "#21212b", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#709343", "#ee7a61", "#ee7a61", "#ee7a61", "#ee7a61", "#ee7a61", "#21212b", "#eac18b", "#eac18b", "#ebd4b8", "#21212b", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#709343", "#ee7a61", "#ee7a61", "#ee7a61", "#ee7a61", "#ee7a61", "#21212b", "#eac18b", "#ebd4b8", "#21212b", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#f19e80", "#ee7a61", "#f19e80", "#ee7a61", "#21212b", "#eac18b", "#21212b", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec", "#21212b", "#c0e49c", "#84af4d", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#d29e76", "#f19e80", "#f5cba7", "#f19e80", "#f5cba7", "#21212b", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#8cd3ec", "#21212b", "#3d6b23", "#21212b", "#21212b", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#d29e76", "#d29e76", "#f19e80", "#f5cba7", "#f19e80", "#f19e80", "#21212b", "#8dd3ec", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8dd3ec"], ["#8cd3ec", "#8cd3ec", "#21212b", "#c0e49c", "#6aa457", "#3d6b23", "#3d6b23", "#21212b", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#d29e76", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#21212b", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec"], ["#8cd3ec", "#8cd3ec", "#21212b", "#c0e49c", "#6aa457", "#6aa457", "#3d6b23", "#3d6b23", "#21212b", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#21212b", "#8dd3ec", "#8cd3ec", "#8cd3ec", "#8cd3ec"], ["#8cd3ec", "#21212b", "#c0e49c", "#a2cf7a", "#a2cf7a", "#6aa457", "#3d6b23", "#3d6b23", "#21212b", "#84af4d", "#709343", "#709343", "#709343", "#d29e76", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#21212b", "#8cd3ec", "#8cd3ec", "#8cd3ec"], ["#21212b", "#c0e49c", "#a2cf7a", "#a2cf7a", "#a2cf7a", "#3d6b23", "#3d6b23", "#3d6b23", "#21212b", "#84af4d", "#709343", "#709343", "#d29e76", "#d29e76", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#f19e80", "#21212b", "#8cd3ec", "#8cd3ec", "#8cd3ec"], ["#21212b", "#c0e49c", "#a2cf7a", "#a2cf7a", "#a2cf7a", "#3d6b23", "#3d6b23", "#3d6b23", "#21212b", "#84af4d", "#709343", "#709343", "#d29e76", "#d29e76", "#f5cba7", "#f19e80", "#f5cba7", "#f19e80", "#f5cba7", "#f19e80", "#f5cba7", "#21212b", "#8cd3ec", "#8cd3ec"], ["#c0e49c", "#a2cf7a", "#a2cf7a", "#a2cf7a", "#599148", "#6aa457", "#3d6b23", "#3d6b23", "#21212b", "#84af4d", "#709343", "#709343", "#d29e76", "#d29e76", "#f19e80", "#f5cba7", "#f19e80", "#f5cba7", "#f19e80", "#f5cba7", "#f19e80", "#f5cba7", "#21212b", "#8cd3ec"]]
+        this.data = [
+            ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd2eb", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ea", "#8dd3eb", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3eb", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd2eb", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3eb", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd2ec", "#8dd3eb", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd2ec", "#8dd3ec", "#8dd3eb", "#8dd3ec", "#8dd3ec", "#8dd2ec", "#8dd3ec", "#8dd3eb", "#8dd3ec", "#8dd3ec", "#8dd2ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8dd2ec", "#8dd2ec", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#8dd3ec", "#8dd2ec", "#8dd3ec", "#8cd2eb", "#8dd2eb", "#8dd3ec", "#8dd2ec", "#8dd3ec", "#8dd2eb", "#8cd2eb", "#8dd3ec", "#8dd2eb", "#8dd3ec", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#8dd2eb", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#20222a", "#1f222b", "#1f222b", "#20212b", "#20212b", "#1f222b", "#1f222b", "#1f222b", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd2eb", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8fd2eb", "#20202a", "#df4134", "#f26836", "#ef6a34", "#ef6937", "#ee6836", "#ee6934", "#ee6933", "#f06936", "#22212a", "#8fd2eb", "#8dd2eb", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#21212c", "#dd4235", "#f26836", "#f26836", "#e4bc93", "#e4bc93", "#e4bc93", "#e4bc93", "#e4bc93", "#f06936", "#da4533", "#21212c", "#8dd3ec", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd2ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#21212c", "#dd4235", "#dd4238", "#f16934", "#f18936", "#e4bc93", "#dad7cf", "#2e3034", "#bbbca8", "#dad7cf", "#f36835", "#20222a", "#d8ceb6", "#21212c", "#21212e", "#21212e", "#8cd3ee", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#21212c", "#dd4235", "#f26836", "#f16934", "#f18936", "#e4bc93", "#dad7cf", "#2d2f33", "#2c3033", "#dad7cf", "#21212c", "#d8ceb6", "#d8ceb6", "#dee2cf", "#dee2cf", "#dee2cf", "#21212a", "#8dd2ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8fd2eb", "#21212c", "#dd4235", "#f26836", "#f26836", "#f16834", "#f18936", "#e5bd94", "#dbd7d0", "#dad7cf", "#dad7cf", "#dad7cf", "#21212c", "#d8ceb6", "#d8ceb6", "#dee2cf", "#dee2cf", "#dee2cf", "#21212a", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8bd5e5", "#24202a", "#dd4235", "#f26836", "#f26836", "#f16834", "#f18936", "#e4bc93", "#e0e1dc", "#e0e1dc", "#e0e1dc", "#e0e1da", "#20212c", "#daccb8", "#daccb8", "#dee2ce", "#dee2cf", "#dee2cf", "#21212a", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#24202c", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f16834", "#f18936", "#e4bc93", "#e0e1dc", "#e0e1dc", "#e0e1db", "#21212c", "#29373e", "#21212c", "#21212c", "#20212b", "#20212b", "#dee2cf", "#21212a", "#8dd2ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#241f2d", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f16934", "#f28a36", "#e4bc93", "#e0e1da", "#e0e1da", "#e0e1dc", "#22212a", "#28363d", "#29373c", "#424240", "#21212e", "#21212e", "#21212c", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#241f2d", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f16934", "#f18936", "#e8bc91", "#e7bc90", "#e7bb8f", "#e7bb8f", "#e7bb8f", "#202226", "#28373d", "#1c2227", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#8dd3ec", "#8dd2ec"], ["#8dd3ec", "#8dd2eb", "#8dd3ec", "#8dd3ec", "#241f2d", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f16934", "#f18936", "#f18936", "#f18936", "#f18936", "#f18936", "#f18936", "#e4bc93", "#1f2228", "#8dd3ec", "#8dd3ec", "#8cd2eb", "#8dd3ec", "#8cd2eb", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#24202c", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f06938", "#f08a36", "#f08a36", "#f08a36", "#f08a36", "#f28936", "#f18936", "#e7bb92", "#202229", "#8dd3ea", "#8dd3ec", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8dd3ec", "#8dd3ec", "#8cd2eb", "#24202c", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f26836", "#f26836", "#f06936", "#f26836", "#f26836", "#f26836", "#f18936", "#f18936", "#e7ba95", "#21212c", "#8cd2eb", "#8dd3ea", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#91d2eb", "#8fd2ec", "#20212a", "#964a3c", "#20212a", "#20212a", "#dc4335", "#f26836", "#f26836", "#f26836", "#f26836", "#f26836", "#f26836", "#f66634", "#f08a35", "#f18936", "#e2bd96", "#22212a", "#8fd2ec", "#8dd3ec", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8fd2ec", "#21212c", "#97493b", "#964a3b", "#b25d4a", "#b25d4a", "#222127", "#db4337", "#f26838", "#f26838", "#f26838", "#f26836", "#f26836", "#f26836", "#f26836", "#f18939", "#f08a39", "#e4bc93", "#1e2229", "#8ed2ed", "#8dd3ec", "#8dd3ec", "#8dd3ec"], ["#8dd3ec", "#8ed1eb", "#21212c", "#97493b", "#b25d4a", "#b25d4a", "#b25d4a", "#eb6b3b", "#21212c", "#dd4235", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f26836", "#f26836", "#f26836", "#f18936", "#f18936", "#e4bc93", "#21212c", "#8dd3ec", "#8cd2eb", "#8dd3ec"], ["#8dd3e9", "#1f222c", "#97493b", "#b25d4a", "#b25d4a", "#b25d4a", "#eb6b3b", "#eb6b3b", "#21212c", "#dd4235", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f26836", "#f26836", "#f06936", "#f06936", "#f18936", "#f18936", "#e4bc93", "#21212c", "#8dd3ec", "#8dd3ec"], ["#21212c", "#97493b", "#97493b", "#b25d4a", "#b25d4a", "#b25d4a", "#eb6b3b", "#ec6b3b", "#21212c", "#dd4235", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f26836", "#f26836", "#f06936", "#f06936", "#f18936", "#f18936", "#e4bc93", "#21212c", "#8dd3ec", "#8dd3ec"], ["#20222a", "#b25d4a", "#b25d4a", "#b25d4a", "#b25d4a", "#b25d4a", "#eb6b3b", "#eb6b3b", "#21212c", "#dd4235", "#dd4235", "#dd4235", "#f26836", "#f26836", "#f26836", "#f26836", "#f16836", "#f26836", "#f26836", "#f18936", "#f18936", "#e4bc93", "#21212a", "#8dd2ec"], ["#974a3c", "#b15e4a", "#b15e4a", "#b15e4a", "#b15e4a", "#b15e4a", "#eb6b3d", "#eb6b3d", "#20212b", "#dc4237", "#dc4237", "#dc4237", "#ef6938", "#ef6938", "#ef6938", "#ef6938", "#ef6938", "#ef6938", "#ef6938", "#f18a37", "#f18a37", "#e2bd95", "#23202b", "#8ed3ea"]
+        ]
         this.dataNull = new Array(this.gridNum).fill(null).map(() => new Array(this.gridNum).fill(null))
     }
 
     #initColorPicker() {
-        document.getElementById('color-picker').addEventListener('change', event => this.color = event.target.value)
+        document.getElementById('color-picker').addEventListener('change', event => {
+            this.color = event.target.value
+        })
     }
 
     #initWrapper() {
@@ -129,7 +133,7 @@ class PixelImage {
         this.data[y][x] = color
     }
 
-    import() {
+    importData() {
         this.dispose()
         const element = document.getElementById('input-file')
         element.click()
@@ -168,7 +172,28 @@ class PixelImage {
         })
     }
 
-    export(filename = 'pixel-image-data.json') {
+    importImage() {
+        const input = document.getElementById('input-file')
+        input.click()
+        input.addEventListener('input', event => {
+            const file = event?.target?.files?.[0]
+            const fileReader = new FileReader()
+            fileReader.readAsDataURL(file)
+            fileReader.onload = e => {
+                const base64 = e.target.result
+                const context = this.context
+                const image = new Image()
+                image.src = base64
+                image.onload = () => {
+                    context.drawImage(image, 0, 0, this.canvasLength, this.canvasLength)
+                    this.image2data()
+                    this.updateCanvas()
+                }
+            }
+        })
+    }
+
+    exportData(filename = 'pixel-image-data.json') {
         const element = document.createElement('a')
         element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(this.data)));
         element.setAttribute('download', filename);
@@ -176,6 +201,13 @@ class PixelImage {
         document.body.appendChild(element);
         element.click();
         document.body.removeChild(element);
+    }
+
+    exportImage(filename = 'pixel-image.png') {
+        const element = document.createElement('a')
+        element.download = filename
+        element.href = document.getElementById('canvas').toDataURL()
+        element.click()
     }
 
     updateCanvas() {
@@ -200,6 +232,52 @@ class PixelImage {
         this.data = this.dataNull
         this.dataLayer = this.dataNull
         this.updateCanvas()
+    }
+
+    rgb2hex(rgb) {
+        rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+        return (rgb && rgb.length === 4) ? "#" +
+            ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+            ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+    }
+
+    image2data() {
+        const context = this.context
+        const { data: imageData } = context.getImageData(0, 0, this.canvasLength, this.canvasLength)
+        const imageRgbData = []
+        for (let i = 0; i < imageData.length; i++) {
+            if (i % 4 === 0) {
+                imageRgbData.push(`rgb(${imageData[i]},${imageData[i + 1]},${imageData[i + 2]},255)`)
+            }
+        }
+        const rawLength = Math.sqrt(imageRgbData.length)
+        const matrix = []
+        let line = -1
+        for (let i = 0; i < imageRgbData.length; i++) {
+            if (i % rawLength === 0) {
+                line++
+                matrix[line] = []
+            }
+            matrix[line].push(imageRgbData[i])
+        }
+        const data = []
+        const skipNum = rawLength / this.gridNum
+        for (let i = 0; i < matrix.length; i++) {
+            if (i % skipNum) { continue }
+            const x = Math.floor(i / skipNum)
+            data[x] = []
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (j % skipNum) { continue }
+                const y = Math.floor(j / skipNum)
+                data[x][y] = this.rgb2hex(matrix[i + skipNum / 2][j + skipNum / 2])
+            }
+        }
+        this.data = data
+    }
+
+    showData() {
+        console.log(this.data)
     }
 
 }
